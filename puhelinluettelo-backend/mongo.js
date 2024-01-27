a61c61e503e4f6826e6bada055d9e25629e84170
juusokasperi <juusor@gmail.com>
2024-01-27 23:21:14 +0200

    mongoose

---
+++ puhelinluettelo-backend/mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<5) {
+  console.log('give password, name and number as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://juusor:${password}@cluster0.umtp8ku.mongodb.net/phoneBook?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: `${process.argv[3]}`,
+  number: `${process.argv[4]}`,
+})
+
+person.save().then(result => {
+  console.log(`Added ${person.name}, number ${person.number} to phonebook`)
+  mongoose.connection.close()
+})
