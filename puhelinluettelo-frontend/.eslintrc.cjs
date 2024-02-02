16bf3ded6116def6cd3b05e33df020d130578061
juusokasperi <juusor@gmail.com>
2024-02-02 18:58:48 +0200

    lint config muutos

--- puhelinluettelo-frontend/.eslintrc.cjs
+++ puhelinluettelo-frontend/.eslintrc.cjs
@@ -1,54 +1,55 @@
 module.exports = {
-    'settings': {
-        'react': {
-            'version': 'detect'
-        }
-    },
-    'env': {
-        'browser': true,
-        'commonjs': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended'
-    ],
-    'overrides': [
-        {
-            'env': {
-                'node': true
-            },
-            'files': [
-                '.eslintrc.{js,cjs}'
-            ],
-            'parserOptions': {
-                'sourceType': 'script'
-            }
-        }
-    ],
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'never'
-        ]
+  'settings': {
+    'react': {
+      'version': 'detect'
     }
+  },
+  'env': {
+    'browser': true,
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
 }
--- puhelinluettelo-frontend/src/services/persons.js
+++ puhelinluettelo-frontend/src/services/persons.js
@@ -3,25 +3,25 @@ import axios from 'axios'
 const baseUrl = '/api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then((response) => response.data)
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
 }
 
 const create = (nameObject) => {
-    const request = axios.post(baseUrl, nameObject)
-    return request.then((response) => response.data)
+  const request = axios.post(baseUrl, nameObject)
+  return request.then((response) => response.data)
 }
 
 const remove = (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then((response) => response.data)
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
 }
 
 const update = (id, updatedObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, updatedObject)
-    return request.then((response) => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, updatedObject)
+  return request.then((response) => response.data)
 }
 
 export default {
-    getAll, create, remove, update,
+  getAll, create, remove, update,
 }
